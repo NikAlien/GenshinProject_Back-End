d660dffddafeff8fbaaeadbb19b77522947edc8a
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-04-04 21:28:01 +0300

    Simple retrieval operations

--- src/main/java/genshin/project/back/end/characters/Controller/CharacterController.java
+++ src/main/java/genshin/project/back/end/characters/Controller/CharacterController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/characters")
@@ -20,8 +22,19 @@ public class CharacterController {
     @Autowired
     private CharacterService service;
 
+
+    //    Get all or only one entity     //
     @GetMapping
     public ResponseEntity<List<Character>> getAllCharacters(){
         return new ResponseEntity<>(service.getAllCharacters(), HttpStatus.OK);
     }
+
+    @GetMapping("/id_{id}")
+    public ResponseEntity<Optional<Character>> getCharacterById(@PathVariable(name = "id") Integer id) {
+        return new ResponseEntity<>(service.getCharacterById(id), HttpStatus.OK);
+    }
+
+    //     Inserting new Character     //
+
+
 }
