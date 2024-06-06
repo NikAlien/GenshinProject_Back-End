c87e7fb13caae6d0c4cac0396991bcfc01fce5c8
NikAlien <115424518+NikAlien@users.noreply.github.com>
2024-06-06 23:26:16 +0300

    weapon - additional operations

--- src/main/java/genshin/project/back/end/characters/Model/Weapon.java
+++ src/main/java/genshin/project/back/end/characters/Model/Weapon.java
@@ -1,16 +1,20 @@
 package genshin.project.back.end.characters.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
 @Data
 @Table(name = "Weapons")
+@AllArgsConstructor
 @NoArgsConstructor
 public class Weapon {
 
@@ -23,7 +27,8 @@ public class Weapon {
     private double secondStatNumbers;
 
     @OneToMany(mappedBy = "weapon")
-    private Set<Character> characterList;
+    @JsonIgnore
+    private Set<Character> characterList = new HashSet<>();
 
     public Weapon(String name, String type, int baseAttack, String additionalCharacteristic, double characteristicNumbers) {
         this.name = name;
@@ -31,6 +36,14 @@ public class Weapon {
         this.baseAttack = baseAttack;
         this.secondStat = additionalCharacteristic;
         this.secondStatNumbers = characteristicNumbers;
-        this.characterList = new HashSet<>();
+    }
+
+    public Weapon(int weaponId, String name, String type, int baseAttack, String secondStat, double secondStatNumbers) {
+        this.weaponId = weaponId;
+        this.name = name;
+        this.type = type;
+        this.baseAttack = baseAttack;
+        this.secondStat = secondStat;
+        this.secondStatNumbers = secondStatNumbers;
     }
 }
